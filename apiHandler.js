f7ee101340ae116a299677afd7c02e772c78c084
andmana <iam.andmana@gmail.com>
2025-02-14 15:16:57 +0700

    Add handler api and get geo location API

---
+++ apiHandler.js
@@ -0,0 +1,19 @@
+export async function getLocationOptions(query = "lon") {
+    try {
+        const response = await fetch(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=10&language=en&format=json`
+        );
+        const dataObj = await response.json();
+        const data = await dataObj.results.map((obj) => ({
+            name: obj.name,
+            country: obj.country,
+            latitude: obj.latitude,
+            longitude: obj.longitude,
+        }));
+        // const data = await dataObj.results;
+        // console.log(data);
+        return data;
+    } catch (e) {
+        console.log(`error : ` + e.message);
+    }
+}
